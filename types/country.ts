6b06b02aa6666d5bde04eec2af8243d832fb9731
Gabriel Amaral <gabrielvieira2205@gmail.com>
2026-04-18 14:27:59 -0300

    Adiciona tipo Country com propriedades relevantes para dados de países

---
+++ types/country.ts
@@ -0,0 +1,12 @@
+export type Country = {
+  slug: string
+  name: string
+  flagUrl: string
+  flagAlt: string
+  capital: string
+  continent: string
+  language: string
+  currency: string
+  population: number
+  populationLabel: string
+}
